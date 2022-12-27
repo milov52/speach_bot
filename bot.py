4f495b70d4792058ea8972830c7eec8502138119
killerarz <milov52@gmail.com>
2022-12-28 00:27:04 +0300

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import logging
+import os
 
+from dotenv import load_dotenv
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
@@ -10,31 +12,60 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+project_id = 'speech-372919'
+language_code = 'ru'
+load_dotenv()
+
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    from google.cloud import dialogflow
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    for text in texts:
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+        return response.query_result.fulfillment_text
+
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Здравствуйте')
 
 
 def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+    text = [update.message.text]
+    update.message.reply_text(detect_intent_texts(project_id,
+                                                  update.effective_user.id,
+                                                  text,
+                                                  language_code))
 
 
 def main() -> None:
     updater = Updater("5972475888:AAH5Ts4lyv-8PwsVhRuR2wdhRtjGyG7rY4s")
     dispatcher = updater.dispatcher
-
-    # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
-
-    # on non command i.e message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
