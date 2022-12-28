5245586f1ed785234014079f4b4f0fed08a8fff7
killerarz <milov52@gmail.com>
2022-12-28 15:06:30 +0300

    add DialogFlow to VK bot.

--- vk_bot.py
+++ vk_bot.py
@@ -4,13 +4,47 @@ from dotenv import load_dotenv
 
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
+from google.cloud import dialogflow
+
+LANGUAGE_CODE = 'ru'
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+
+    return response.query_result.fulfillment_text
 
 
 def echo(event, vk_api):
+    project_id = os.getenv("PROJECT_ID")
+    answer = detect_intent_texts(project_id,
+                                 event.user_id,
+                                 event.text,
+                                 LANGUAGE_CODE)
+    print(answer)
+
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1,1000)
+        message=answer,
+        random_id=random.randint(1, 1000)
     )
 
 
